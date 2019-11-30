47ef745a1a1ee39edc4b30be017c9dd0eea90732
Adam Hoge <adhoge@gmail.com>
2019-11-30 11:39:28 -0500

    Added EF Core

--- StackCats.Web/Startup.cs
+++ StackCats.Web/Startup.cs
@@ -6,9 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackCats.Web.Context;
+using StackCats.Web.Repositories;
 
 namespace StackCats.Web
 {
@@ -24,6 +27,11 @@ namespace StackCats.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ReleaseNotificationRequestContext>(item =>
+            item.UseMySql(Configuration.GetConnectionString("MariaDb")));
+
+            services.AddScoped<IReleaseNotificationRequestRepository, ReleaseNotificationRequestRepository>();
+
             services.AddControllersWithViews();
         }
 
